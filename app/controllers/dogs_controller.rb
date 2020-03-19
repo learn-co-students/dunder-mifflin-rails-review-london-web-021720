ffce247fe0c84cf17836ccb9348004f5f7918fa9
Amaterasus <skyetay03@googlemail.com>
2020-03-19 12:25:25 +0000

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,17 @@
 class DogsController < ApplicationController
+    before_action :set_dog, only: :show
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+    end
+
+    private
+
+    def set_dog
+        @dog = Dog.find_by(id: params[:id])
+        @dog_employees = @dog.employees
+    end
 end
